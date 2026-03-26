b5cc0fd2192ed5235f9f748bc39ebef838ad52a2
weiyi <791422171@qq.com>
2026-03-26 11:24:23 +0800

    feat: 优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 export * from './types';
 
-export { AuthAPI } from './auth';
 export { GenerateAPI } from './generate';
 export { MeAPI } from './me';
 export { PaymentAPI } from './payment';
 export { UploadAPI } from './upload';
 export { DefaultAPI } from './default';
+export { AuthAPI } from './auth';
